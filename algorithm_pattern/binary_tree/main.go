1415376cbb689f89c0748d971881d7b84b625b16
cuiks <1253016652@qq.com>
2021-06-08 17:44:20 +0800

    20210608

--- algorithm_pattern/binary_tree/main.go
+++ algorithm_pattern/binary_tree/main.go
@@ -332,6 +332,52 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	return nil
 }
 
+/*
+BFS层次应用
+*/
+// 二叉树自底向上的层次遍历。层次遍历完成后逆序即可
+
+// 二叉树 Z字形遍历
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	result := make([][]int, 0)
+	if root == nil {
+		return result
+	}
+
+	queue := make([]*TreeNode, 0)
+	toggle := false
+
+	for len(queue) > 0 {
+		l := len(queue)
+		list := make([]int, 0)
+		for i := 0; i < l; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			list = append(list, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		if toggle {
+			reverse(list)
+		}
+		result = append(result)
+		toggle = !toggle
+	}
+	return result
+}
+
+func reverse(queue []int) {
+	for i := 0; i < len(queue)/2; i++ {
+		queue[i], queue[len(queue)-i-1] = queue[len(queue)-i-1], queue[i]
+	}
+}
+
+// https://greyireland.gitbook.io/algorithm-pattern/shu-ju-jie-gou-pian/binary_tree#er-cha-sou-suo-shu-ying-yong
+
 func main() {
 
 }
