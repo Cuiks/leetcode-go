a0dbd5c74369248b47e4fb4d903538a7ce9c627f
cuiks <1253016652@qq.com>
2021-10-25 13:38:54 +0800

    feat: daily question

---
+++ answer/240-搜索二维矩阵/main.go
@@ -0,0 +1,20 @@
+package main
+
+func searchMatrix(matrix [][]int, target int) bool {
+	x, y := 0, len(matrix[0])-1
+	for x < len(matrix) && y >= 0 {
+		if matrix[x][y] == target {
+			return true
+		}
+		if matrix[x][y] < target {
+			x++
+		} else {
+			y--
+		}
+	}
+	return false
+}
+
+func main() {
+
+}
